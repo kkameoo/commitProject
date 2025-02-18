03ab01152ff43740be80028365ea8312841d1bf0
lee <l_lpp@naver.com>
2025-02-18 09:54:22 +0900

    kjh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import "../styles/header.css"; // CSS 파일 적용
 import Logo from "../images/logo.jpeg"; // 로컬 이미지 import
 
+
 function Header() {
   const navigate = useNavigate();
   return (
@@ -13,13 +14,7 @@ function Header() {
       <button className="menu-button">고객서비스</button>
       <button className="menu-button">회원테스트</button>
       <button className="menu-button" onClick={() => navigate("/Review")}>회원리뷰</button>
-      <button
-        onClick={() => {
-          navigate("/MyPage");
-        }}
-      >
-        마이페이지
-      </button>
+      <button className="menu-button">마이페이지</button>
       <button className="menu-button" onClick={() => navigate("/SignIn")}>로그인</button>
       <button className="menu-button" onClick={() => navigate("/SignUp")}>회원가입</button>
     </div>
