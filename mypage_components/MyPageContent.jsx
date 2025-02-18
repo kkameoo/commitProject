0515147ef69c593656a10c219165b4c4a4bd9738
kkameoo <khdg1202@naver.com>
2025-02-18 10:25:58 +0900

    commit4

--- mypage_components/MyPageContent.jsx
+++ mypage_components/MyPageContent.jsx
@@ -16,10 +16,46 @@ const Box = styled.div`
   border: 2px solid green;
 `;
 
+const TopBox = styled.div`
+  height: 10rem;
+  border: 2px solid red;
+`;
+const InnerBox = styled.div`
+  width: 20rem;
+  height: 100%;
+  border: 2px solid yellow;
+`;
+const Name = styled.div`
+  border: 2px solid black;
+  height: 2.5rem;
+  text-align: center;
+  padding-top: 1rem;
+`;
+const Grade = styled.div`
+  border: 2px solid black;
+  height: 1.5rem;
+  text-align: center;
+`;
+const Photo = styled.div`
+  border: 2px solid black;
+  height: 4.3rem;
+`;
+const PhotoButton = styled.button``;
+
 function MyPageContent() {
   return (
     <Container>
-      <Box></Box>
+      <Box>
+        <TopBox>
+          <InnerBox>
+            <Name>ㅁㅁㅁ님 안녕하세요</Name>
+            <Grade>회원유형: ㅁㅁㅁ</Grade>
+            <Photo>
+              <PhotoButton></PhotoButton>
+            </Photo>
+          </InnerBox>
+        </TopBox>
+      </Box>
     </Container>
   );
 }
